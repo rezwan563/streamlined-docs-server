2f2df34972b7c99542fc53be62f57f6e17958f72
Rezwan <t.rewan17@gmail.com>
2023-09-01 09:04:28 +0600

    Routes for user and profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,7 +2,23 @@ const router = require("express").Router()
 const Profiles = require("../models/profile");
 
 // create profile info
+router.post("/", async(req,res)=>{
+    const profileData = req.body;
+    try{
+        const newProfileData = new Profiles(profileData);
+        const saveProfileData = await newProfileData.save()
+        res.status(201).json("Profile data saved successfully")
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 // get profile info
-
+router.get("/:userEmail", async(req,res)=>{
+    try{
+        const profileData = await Profiles.findOne({userEmail: req.body.email})
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,21 +1,46 @@
-const router = require("express").Router()
+const router = require("express").Router();
 const Users = require("../models/user");
 
 // create user
 router.post("/", async (req, res) => {
-    const user = req.body;
-    console.log(user);
+  const user = req.body;
+  try {
     const newUser = await Users.findOne({ email: user.email });
     if (newUser) {
-      return res.send({ message: "User already exists" });
+      return res.status(409).json("User already exists");
     }
     const loggedUser = new Users(user);
-    console.log(loggedUser);
     const result = await loggedUser.save();
-    res.send(result)
-})
-// get user
-// update user
-// delete user
+    res.send(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-module.exports = router
+// get user
+router.get("/:email", async (req, res) => {
+  const user = await Users.findById(req.params.email);
+  res.send(user);
+});
+
+// update user
+router.put("/:email", async (req, res) => {
+  try{
+    const updatedDoc = req.body;
+    const updatedUser = await Users.findByIdAndUpdate(req.params.email, updatedDoc)
+    res.send(updatedUser)
+  }catch(err){
+    res.status(500).json(err)
+  }
+});
+// delete user
+router.delete("/:email", async(req,res)=>{
+  try{
+    await Users.findByIdAndDelete(req.params.email)
+    res.status(200).json("Successfully deleted the user")
+  }catch(err){
+    res.status(500).json(err)
+  }
+})
+
+module.exports = router;
