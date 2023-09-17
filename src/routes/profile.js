f008a7f1758a214a2e5c615249af28014f215695
Muhammad Rasel Bin Siddik <muhammadraselbinsiddik@gmail.com>
2023-09-17 14:04:06 +0600

    All profiles get api create

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const profile = require("../models/profile");
 const Profiles = require("../models/profile");
 
+
 // create profile info
 router.post("/", async (req, res) => {
   const profileData = req.body;
@@ -26,5 +27,42 @@ router.get("/:email", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
+router.get("/:email?", async (req, res) => {
+  try {
+    const email = req.params.email;
+    if (email) {
+      // If an email is provided, search for a profile with that email
+      const profileData = await Profiles.findOne({ userEmail: email });
+
+      if (!profileData) {
+        return res.status(404).json({ message: "Profile not found" });
+       
+      }
+      return res.status(200).send(profileData);
+
+    }
+    else {
+      // If no email is provided, return a list of all profiles
+      const allProfiles = await Profiles.find();
+
+      if (!allProfiles || allProfiles.length === 0) {
+        return res.status(404).json({ message: "No profiles found" });
+      }
+      return res.status(200).json(allProfiles);
+     
+    }
+    
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+  
+});
+
+
+
+
 
 module.exports = router;
