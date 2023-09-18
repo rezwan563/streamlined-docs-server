2015c9ddc67632e320e41606fa146eff30f2ba92
Rezwan <t.rewan17@gmail.com>
2023-09-18 20:13:13 +0600

    Deleted jwt

--- index2.js
+++ index2.js
@@ -22,33 +22,6 @@ app.use("/api/users", userRoute)
 app.use("/api/profiles", profileRoute)
 app.use("/api/pending_profiles", pendingApplication)
 
-const verifyJWT = (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if (!authorization) {
-    return res
-      .status(401)
-      .send({ error: true, message: "unauthorized access" });
-  }
-  const token = authorization.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res
-        .status(401)
-        .send({ error: true, message: "unauthorized access" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-};
-
-app.post("/jwt", (req, res) => {
-  const user = req.body;
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1h",
-  });
-  res.send({ token });
-});
-
 
 app.get("/", (req, res) => {
   res.json("Hello from streamlined server");
